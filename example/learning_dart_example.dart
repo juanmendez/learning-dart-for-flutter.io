2912c9adc6efa790753a5cc25a6aaf778f8d8195
Juan Mendez <juan.mendez@grainger.com>
2018-07-06 18:52:44 -0500

    param options

--- example/learning_dart_example.dart
+++ example/learning_dart_example.dart
@@ -1,6 +1,6 @@
 import 'dart:core';
 
-class Band{
+class Band {
   String name;
   String album;
   int year;
@@ -16,4 +16,25 @@ class Band{
 Band greatestBand() => Band(name: "Led Zeppelin", album: "Led Zeppelin I", year: 1968);
 
 //pay attention how to pass a lambda,, in Kotlin we do bandF:()->String
-bool checkBandsName({Band bandF(), bandName:String}) => bandF().name == bandName;
+bool checkBandsName({Band bandF(), bandName: String}) =>
+    bandF().name == bandName;
+
+int sum({int num1, int num2 = 0}) => num1 + num2;
+
+int sumSeveral(int num1, [int num2 = 0, int num3]) {
+  int sum = num1 + num2;
+
+  if (num3 != null) {
+    sum += num3;
+  }
+
+  return sum;
+}
+
+Band makeBandFromParams(String name, [String album, int year = 2018]){
+  Band band = new Band(name: name);
+
+  band.album = album ?? name; //Kotlin uses Elvis ?:, here is ??
+  band.year = year;
+  return band;
+}
--- test/learning_dart_test.dart
+++ test/learning_dart_test.dart
@@ -2,18 +2,32 @@ import 'package:test/test.dart';
 import '../example/learning_dart_example.dart';
 
 void main() {
-
-
   group('Function var', () {
-
     test("passing a method as a param", () {
-       expect( greatestBand().name, equals("Led Zeppelin"));
-       expect( checkBandsName(bandF:greatestBand, bandName: "Led Zeppelin"), equals(true));
+      expect(greatestBand().name, equals("Led Zeppelin"));
+      expect(checkBandsName(bandF: greatestBand, bandName: "Led Zeppelin"),
+          equals(true));
     });
 
-    test( "passing anonymous function", (){
+    test("passing anonymous function", () {
       var b = Band(name: "Deep Purple", album: "Led Zeppelin I", year: 1968);
-      expect( checkBandsName( bandF:()=>b, bandName: "Aerosmith"), equals(false));
+      expect(
+          checkBandsName(bandF: () => b, bandName: "Aerosmith"), equals(false));
+    });
+  });
+
+  group("params features", () {
+    test("default params", () {
+      //num2 is 0 by default
+      expect(sum(num1: 5), equals(5));
+    });
+
+    test("optinal params", () {
+      expect(sumSeveral(1,2,3), equals(6));
+      expect(sumSeveral(1,2), equals(3));
+      expect(sumSeveral(1), equals(1));
+      expect(makeBandFromParams("Aerosmith").album, equals("Aerosmith"));
+      expect(makeBandFromParams("Aerosmith").year, equals(2018));
     });
   });
 }
