84566bcac085a5ea2b896e6308cad133e8e5f018
Juan Mendez <contact@juanmendez.info>
2019-04-21 21:00:24 -0500

    .rxDart_basics:

--- example/learning_dart_example.dart
+++ example/learning_dart_example.dart
@@ -19,7 +19,7 @@ class Band{
  * There are several striking similarities. Unfortunately, Dart's lambdas don't simplify themselves as much
  * as the ones in Kotlin. The story of the Band class is quite the same.
  */
-Future<List<Band>> getBands([bool expectException = false]) {
+Future<List<Band>> getBands([bool expectException = false]) async {
   //async... output is of type Future<List<String>>
 
     var output = new File("./raw/RockBands.csv").readAsLines();
@@ -32,12 +32,13 @@ Future<List<Band>> getBands([bool expectException = false]) {
 
         return lines
                 .skip(1)
-                .map((String line){
+                .map((line){
                     var columns = line.split(",");
                     return new Band( name:columns[0], album:columns[1], year:int.parse(columns[2]));
                 }).toList();
     });
 }
 
-void main(){
+void main() {
+
 }
--- test/learning_dart_test.dart
+++ test/learning_dart_test.dart
@@ -1,28 +1,38 @@
 import 'package:test/test.dart';
 import '../example/learning_dart_example.dart';
+import 'package:rxdart/rxdart.dart';
 
-@Timeout(const Duration(seconds: 30))
 void main() {
-  var artists = Map<String,List<String>>();
-
-  group('A group of tests', () {
-    setUp((){
-        print("group.setUp()");
-    });
-
-    //@see https://www.dartlang.org/tutorials/language/futures
-    //@Test `Check result`
-    test("asynchronous call", () async {
-      var bands = await getBands();
+  test("load bands", () {
+    Observable<List<Band>>.fromFuture(getBands()).listen((List<Band> bands){
+      var artists = Map<String,List<String>>();
 
       bands.forEach((band){
-         if(artists[band.name] == null){
-             artists[band.name] = List<String>();
-         }
-         artists[band.name].add(band.album);
+        if(artists[band.name] == null){
+          artists[band.name] = List<String>();
+        }
+        artists[band.name].add(band.album);
       });
 
       expect(artists["Bon Jovi"].length, equals(2));
+    }, onError: (Object error){
+      print("$error");
+    }, onDone: (){
+      print("completed");
     });
+  });
+
+  test("get Bon Jovi albums", (){
+    Observable<List<Band>>.fromFuture(getBands(true)).map((bands){
+      bands.where((band)=> band.name == "Bon Jovi");
+    }).listen((List<Band> bands){
+      bands.forEach((b) => print("$b"));
+
+      assert(bands.every((band){
+        band.name == "Bon Jovi";
+      }));
+    }, onError: (error){
+      print("error $error");
+    }, onDone: ()=> print("done"));
   });
 }
